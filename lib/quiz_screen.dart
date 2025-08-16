e485725c5c3fbf5eae24429979defdae2aeaa080
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-08-16 17:23:24 +0530

    style(answer button): add custom shape for answer button

--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuizScreen extends StatefulWidget {
   const QuizScreen({super.key});
@@ -12,6 +13,19 @@ class QuizScreen extends StatefulWidget {
 class _QuizScreenState extends State<QuizScreen> {
   @override
   Widget build(context) {
-    return Text("Quiz Screen");
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Questions", style: TextStyle(color: Colors.white)),
+          SizedBox(height: 30),
+          AnswerButton(answerText: "answer", onTap: () {}),
+          AnswerButton(answerText: "answer", onTap: () {}),
+          AnswerButton(answerText: "answer", onTap: () {}),
+          AnswerButton(answerText: "answer", onTap: () {}),
+        ],
+      ),
+    );
   }
 }
