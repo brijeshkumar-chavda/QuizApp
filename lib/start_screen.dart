5ca9baa96267739dc62101ace353c3a778b429b1
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-08-16 11:54:47 +0530

    style: add the assets path to the pubspec.yaml file

---
+++ lib/start_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key, required this.switchToQuizScreen});
+
+  final void Function() switchToQuizScreen;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300,
+            color: const Color.fromARGB(160, 255, 255, 255),
+          ),
+          SizedBox(height: 30),
+          Text(
+            "Learn Flutter the fun way!",
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+          SizedBox(height: 30),
+          FilledButton.icon(
+            onPressed: switchToQuizScreen,
+            style: FilledButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.purple,
+            ),
+            icon: const Icon(Icons.arrow_right_alt, size: 34),
+            label: Text(
+              "Start Quiz",
+              style: const TextStyle(fontSize: 24, color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
