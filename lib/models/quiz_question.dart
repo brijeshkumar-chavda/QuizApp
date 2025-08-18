587f32a1530d4e484e5696272e1a5bdea6064de4
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-08-18 15:56:22 +0530

    feat(quiz questions): add function for shuffling the answers
    HOW: use the List.of() method from the list class for creating the copy of the answers list and use the .shuffle() method to randomize it.

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -2,4 +2,10 @@ class QuizQuestion {
   const QuizQuestion({required this.question, required this.answers});
   final String question;
   final List<String> answers;
+
+  List<String> getShuffledAnswer() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
