6ec09f038961dd48997668b513dbf54ed7b5f81e
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-08-18 12:19:42 +0530

    fix(quiz screen): answer text is not showing for options

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Quiz App
+
+# Progress
+Start:
+End:
+
+
+18-08-2025
+11:30 - 
--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuizScreen extends StatefulWidget {
   const QuizScreen({super.key});
@@ -13,17 +14,19 @@ class QuizScreen extends StatefulWidget {
 class _QuizScreenState extends State<QuizScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Questions", style: TextStyle(color: Colors.white)),
+          Text(currentQuestion.question, style: TextStyle(color: Colors.white)),
           SizedBox(height: 30),
-          AnswerButton(answerText: "answer", onTap: () {}),
-          AnswerButton(answerText: "answer", onTap: () {}),
-          AnswerButton(answerText: "answer", onTap: () {}),
-          AnswerButton(answerText: "answer", onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
         ],
       ),
     );
